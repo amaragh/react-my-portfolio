b871d6ba4865e16a28ddbb80c9016156a13d9e2a
amaragh <mercer.alecia@gmail.com>
2023-01-11 22:01:42 -0600

    create footer component

---
+++ react-my-portfolio/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='container text-center'>
+            <footer className='row justify-content-evenly'>
+                <a href="#" className='col'>GitHub</a>
+                <a href="#" className='col'>LinkedIn</a>
+                <a href="#" className='col'>LEGO</a>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
