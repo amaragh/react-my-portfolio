da910d34e5538a207ee135b0eab1efcc5f6f0274
amaragh <mercer.alecia@gmail.com>
2023-01-11 22:02:08 -0600

    start about section

---
+++ react-my-portfolio/src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <img src='#' alt='Avatar image'/>
+        </div>
+    )
+}
+
+export default About;
