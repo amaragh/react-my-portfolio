c8a25b4008c49b724e8c434d62a7a217d0d3178b
amaragh <mercer.alecia@gmail.com>
2023-01-15 15:40:16 -0600

    add github repo links for projects

--- react-my-portfolio/src/components/Project.js
+++ react-my-portfolio/src/components/Project.js
@@ -6,28 +6,32 @@ function Project() {
         {
             name: 'Farmers Market',
             languages: 'Node',
+            repoLink: 'https://github.com/fdeaquino/Farmers_Market'
         },
         {
             name: 'Tempo Run',
             languages: 'HTML CSS',
+            repoLink: 'https://github.com/amaragh/tempo-run-songfinder'
         },
         {
             name: 'Run Buddy',
             languages: 'HTML CSS',
-            appLink: '',
-            repoLink: ''
+            repoLink: 'https://github.com/amaragh/run-buddy'
         },
         {
             name: 'Weather Dashboard',
             languages: 'HTML CSS',
+            repoLink: 'https://github.com/amaragh/weather-dashboard'
         },
         {
             name: 'Note Taker',
             languages: 'Node',
+            repoLink: 'https://github.com/amaragh/note-taker'
         },
         {
             name: 'Tech Blog',
             languages: 'HTML CSS',
+            repoLink: 'https://github.com/amaragh/tech-blog-mvc'
         }
     ];
 
@@ -35,17 +39,15 @@ function Project() {
         <div className='row justify-content-center'>
             {projects.map((project) =>
                 <section className='project col-md-10 col-lg-6' >
-
                     <img
                         src={require(`../assets/images/${project.name}.png`)}
                         alt={project.name}
                         key={project.name}
                         className='col-12'
                     />
+                    <h3>{project.name}</h3>
+                    <p>This is a description of the {project.name} project. Click <a href={project.repoLink} target="_blank" rel="noreferrer">here</a> to see Github repo.</p>
                 </section>
-
-
-
             )}
 
         </div>
