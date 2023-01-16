2658418305708e83fa10b295df74309606b6bb4b
amaragh <mercer.alecia@gmail.com>
2023-01-15 20:13:40 -0600

    add link to pdf resume, add styling

--- react-my-portfolio/src/components/pages/Resume.js
+++ react-my-portfolio/src/components/pages/Resume.js
@@ -1,30 +1,29 @@
 import React from 'react'
-
 function Resume() {
     return (
-        <div>
-            <h1>Resume
-                <a href='#'>Download Resume</a>
-            </h1>
+        <div className='resume'>
+            <div className='resume-header'>
+                <h1>Resume</h1>
+                <a href='https://drive.google.com/file/d/1Kpwn7Y7ALDPw1sfmwBot2wSswp77TKtP/view?usp=share_link'
+                    target="_blank" rel="noreferrer"> Click here to download resume</a>
+            </div>
             <div>
                 <h2>Front-End Proficiencies</h2>
                 <ul>
                     <li>HTML</li>
                     <li>CSS</li>
-                    <li>HTML</li>
-                    <li>HTML</li>
-                    <li>HTML</li>
-                    <li>HTML</li>
+                    <li>Bootstrap</li>
+                    <li>Node.js</li>
+                    <li>React.js</li>
                 </ul>
             </div>
             <div>
                 <h2>Back-End Proficiencies</h2>
                 <ul>
-                    <li>HTML</li>
-                    <li>HTML</li>
-                    <li>HTML</li>
-                    <li>HTML</li>
-                    <li>HTML</li>
+                    <li>Node.js</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>Express.js</li>
                 </ul>
             </div>
         </div>
