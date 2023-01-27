bb754708d4a4e92d0128645142874f60fa0a75ef
amaragh <mercer.alecia@gmail.com>
2023-01-26 21:26:08 -0600

    added new project and links to the deployed applications

--- react-my-portfolio/src/components/Project.js
+++ react-my-portfolio/src/components/Project.js
@@ -6,32 +6,44 @@ function Project() {
         {
             name: 'Farmers Market',
             languages: 'Node',
-            repoLink: 'https://github.com/fdeaquino/Farmers_Market'
+            repoLink: 'https://github.com/fdeaquino/Farmers_Market',
+            appLink: 'https://byte-farmers-market.herokuapp.com/'
         },
         {
             name: 'Tempo Run',
             languages: 'HTML CSS',
-            repoLink: 'https://github.com/amaragh/tempo-run-songfinder'
+            repoLink: 'https://github.com/amaragh/tempo-run-songfinder',
+            appLink: 'https://amaragh.github.io/tempo-run-songfinder/'
+        },
+        {
+            name: 'Brick Street',
+            languages: 'MERN',
+            repoLink: 'https://github.com/Steezy1416/brickstreet',
+            appLink: 'https://brickstreet.herokuapp.com/'
         },
         {
             name: 'Run Buddy',
             languages: 'HTML CSS',
-            repoLink: 'https://github.com/amaragh/run-buddy'
+            repoLink: 'https://github.com/amaragh/run-buddy',
+            appLink: 'https://amaragh.github.io/run-buddy/'
         },
         {
             name: 'Weather Dashboard',
             languages: 'HTML CSS',
-            repoLink: 'https://github.com/amaragh/weather-dashboard'
+            repoLink: 'https://github.com/amaragh/weather-dashboard',
+            appLink: 'https://amaragh.github.io/weather-dashboard/'
         },
         {
             name: 'Note Taker',
             languages: 'Node',
-            repoLink: 'https://github.com/amaragh/note-taker'
+            repoLink: 'https://github.com/amaragh/note-taker',
+            appLink: 'https://note-taker-876.herokuapp.com/'
         },
         {
             name: 'Tech Blog',
             languages: 'HTML CSS',
-            repoLink: 'https://github.com/amaragh/tech-blog-mvc'
+            repoLink: 'https://github.com/amaragh/tech-blog-mvc',
+            appLink: 'https://the-tech-blog-876.herokuapp.com/'
         }
     ];
 
@@ -40,7 +52,7 @@ function Project() {
             {projects.map((project) =>
                 <section className='project col-md-10 col-lg-6 text-center' >
                     <h4>{project.name}</h4>
-                    <p>This is a description of the {project.name} project. Click <a href={project.repoLink} target="_blank" rel="noreferrer">here</a> to see Github repo.</p>
+                    <p>This is a description of the {project.name} project. Click <a href={project.repoLink} target="_blank" rel="noreferrer">here</a> to see Github repo. Click <a href={project.appLink} target="_blank" rel="noreferrer">here</a> to see the deployed application.</p>
                     <img
                         src={require(`../assets/images/${project.name}.png`)}
                         alt={project.name}
